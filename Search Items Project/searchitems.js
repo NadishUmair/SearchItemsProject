7fde0c9ef29094e2043433e735913c70a57473ab
NadishUmair <129198513+NadishUmair@users.noreply.github.com>
2023-12-30 00:44:22 +0500

    Add files via upload

---
+++ Search Items Project/searchitems.js
@@ -0,0 +1,293 @@
+const products=[{"id":1,
+"title":"iPhone 9",
+"description":"An apple mobile which is nothing like apple",
+"price":549,
+"discountPercentage":12.96,
+"rating":4.69,"stock":94,
+"brand":"Apple","category":"smartphones",
+"thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/1/1.jpg",
+"https://i.dummyjson.com/data/products/1/2.jpg",
+"https://i.dummyjson.com/data/products/1/3.jpg",
+"https://i.dummyjson.com/data/products/1/4.jpg",
+"https://i.dummyjson.com/data/products/1/thumbnail.jpg"]},
+{"id":12,"title":"Brown Perfume",
+"description":"Royal_Mirage Sport Brown Perfume for Men & Women - 120ml",
+"price":40,
+"discountPercentage":15.66,"rating":4,"stock":52,"brand":"Royal_Mirage",
+"category":"fragrances","thumbnail":"https://i.dummyjson.com/data/products/12/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/12/1.jpg",
+"https://i.dummyjson.com/data/products/12/2.jpg",
+"https://i.dummyjson.com/data/products/12/3.png",
+"https://i.dummyjson.com/data/products/12/4.jpg",
+"https://i.dummyjson.com/data/products/12/thumbnail.jpg"]},
+{"id":13,"title":"Fog Scent Xpressio Perfume",
+"description":"Product details of Best Fog Scent Xpressio Perfume 100ml For Men cool long lasting perfumes for Men",
+"price":13,
+"discountPercentage":8.14,"rating":4.59,"stock":61,"brand":"Fog Scent Xpressio",
+"category":"fragrances","thumbnail":"https://i.dummyjson.com/data/products/13/thumbnail.webp",
+"images":["https://i.dummyjson.com/data/products/13/1.jpg","https://i.dummyjson.com/data/products/13/2.png",
+"https://i.dummyjson.com/data/products/13/3.jpg",
+"https://i.dummyjson.com/data/products/13/4.jpg",
+"https://i.dummyjson.com/data/products/13/thumbnail.webp"]},
+{"id":18,"title":"Oil Free Moisturizer 100ml",
+"description":"Dermive Oil Free Moisturizer with SPF 20 is specifically formulated with ceramides, hyaluronic acid & sunscreen.",
+"price":40,"discountPercentage":13.1,"rating":4.56,"stock":88,
+"brand":"Dermive","category":"skincare",
+"thumbnail":"https://i.dummyjson.com/data/products/18/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/18/1.jpg",
+"https://i.dummyjson.com/data/products/18/2.jpg",
+"https://i.dummyjson.com/data/products/18/3.jpg",
+"https://i.dummyjson.com/data/products/18/4.jpg",
+"https://i.dummyjson.com/data/products/18/thumbnail.jpg"]},
+{"id":19,"title":"Skin Beauty Serum.",
+"description":"Product name: rorec collagen hyaluronic acid white face serum riceNet weight: 15 m",
+"price":46,"discountPercentage":10.68,"rating":4.42,
+"stock":54,
+"brand":"ROREC White Rice",
+"category":"skincare","thumbnail":"https://i.dummyjson.com/data/products/19/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/19/1.jpg",
+"https://i.dummyjson.com/data/products/19/2.jpg",
+"https://i.dummyjson.com/data/products/19/3.png",
+"https://i.dummyjson.com/data/products/19/thumbnail.jpg"]},
+{"id":20,
+"title":"Freckle Treatment Cream- 15gm",
+"description":"Fair & Clear is Pakistan's only pure Freckle cream which helpsfade Freckles, Darkspots and pigments. Mercury level is 0%, so there are no side effects.",
+"price":70,
+"discountPercentage":16.99,
+"rating":4.06,
+"stock":140,
+"brand":"Fair & Clear",
+"category":"skincare",
+"thumbnail":"https://i.dummyjson.com/data/products/20/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/20/1.jpg",
+"https://i.dummyjson.com/data/products/20/2.jpg",
+"https://i.dummyjson.com/data/products/20/3.jpg",
+"https://i.dummyjson.com/data/products/20/4.jpg",
+"https://i.dummyjson.com/data/products/20/thumbnail.jpg"]},
+{"id":21,"title":"- Daal Masoor 500 grams","description":"Fine quality Branded Product Keep in a cool and dry place","price":20,"discountPercentage":4.81,
+"rating":4.44,"stock":133,"brand":"Saaf & Khaas","category":"groceries","thumbnail":"https://i.dummyjson.com/data/products/21/thumbnail.png","images":["https://i.dummyjson.com/data/products/21/1.png","https://i.dummyjson.com/data/products/21/2.jpg","https://i.dummyjson.com/data/products/21/3.jpg"]},
+{"id":22,"title":"Elbow Macaroni - 400 gm","description":"Product details of Bake Parlor Big Elbow Macaroni - 400 gm","price":14,
+"discountPercentage":15.58,"rating":4.57,"stock":146,
+"brand":"Bake Parlor Big","category":"groceries",
+"thumbnail":"https://i.dummyjson.com/data/products/22/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/22/1.jpg",
+"https://i.dummyjson.com/data/products/22/2.jpg",
+"https://i.dummyjson.com/data/products/22/3.jpg"]},
+{"id":23,"title":"Orange Essence Food Flavou","description":"Specifications of Orange Essence Food Flavour For Cakes and Baking Food Item","price":14,"discountPercentage":8.04,
+"rating":4.85,
+"stock":26,
+"brand":"Baking Food Items","category":"groceries",
+"thumbnail":"https://i.dummyjson.com/data/products/23/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/23/1.jpg",
+"https://i.dummyjson.com/data/products/23/2.jpg",
+"https://i.dummyjson.com/data/products/23/3.jpg",
+"https://i.dummyjson.com/data/products/23/4.jpg",
+"https://i.dummyjson.com/data/products/23/thumbnail.jpg"]},
+{"id":2,
+"title":"iPhone X",
+"description":"SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+"price":899,
+"discountPercentage":17.94,
+"rating":4.44,"stock":34,
+"brand":"Apple",
+"category":"smartphones",
+"thumbnail":"https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/2/1.jpg",
+"https://i.dummyjson.com/data/products/2/2.jpg",
+"https://i.dummyjson.com/data/products/2/3.jpg",
+"https://i.dummyjson.com/data/products/2/thumbnail.jpg"]},
+{"id":3,
+"title":"Samsung Universe 9",
+"description":"Samsung's new variant which goes beyond Galaxy to the Universe",
+"price":1249,
+"discountPercentage":15.46,
+"rating":4.09,"stock":36,
+"brand":"Samsung","category":"smartphones",
+"thumbnail":"https://i.dummyjson.com/data/products/3/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/3/1.jpg"]},
+{"id":4,
+"title":"OPPOF19",
+"description":"OPPO F19 is officially announced on April 2021.",
+"price":280,"discountPercentage":17.91,
+"rating":4.3,
+"stock":123,
+"brand":"OPPO",
+"category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/4/1.jpg",
+"https://i.dummyjson.com/data/products/4/2.jpg",
+"https://i.dummyjson.com/data/products/4/3.jpg","https://i.dummyjson.com/data/products/4/4.jpg",
+"https://i.dummyjson.com/data/products/4/thumbnail.jpg"]},
+{"id":5,"title":"Huawei P30",
+"description":"Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+"price":499,"discountPercentage":10.58,
+"rating":4.09,
+"stock":32,
+"brand":"Huawei",
+"category":"smartphones",
+
+"thumbnail":"https://i.dummyjson.com/data/products/5/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/5/1.jpg",
+"https://i.dummyjson.com/data/products/5/2.jpg","https://i.dummyjson.com/data/products/5/3.jpg"]},
+{"id":6,"title":"MacBook Pro","description":"MacBook Pro 2021 with mini-LED display may launch between September, November",
+"price":1749,
+"discountPercentage":11.02,
+"rating":4.57,"stock":83,"brand":"Apple",
+"category":"laptops",
+"thumbnail":"https://i.dummyjson.com/data/products/6/thumbnail.png",
+"images":["https://i.dummyjson.com/data/products/6/1.png",
+"https://i.dummyjson.com/data/products/6/2.jpg",
+"https://i.dummyjson.com/data/products/6/3.png",
+"https://i.dummyjson.com/data/products/6/4.jpg"]},
+{"id":7,"title":"Samsung Galaxy Book",
+"description":"Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched","price":1499,"discountPercentage":4.15,
+"rating":4.25,
+"stock":50,
+"brand":"Samsung",
+"category":"laptops","thumbnail":"https://i.dummyjson.com/data/products/7/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/7/1.jpg","https://i.dummyjson.com/data/products/7/2.jpg","https://i.dummyjson.com/data/products/7/3.jpg","https://i.dummyjson.com/data/products/7/thumbnail.jpg"]},{"id":8,"title":"Microsoft Surface Laptop 4","description":"Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+"price":1499,
+"discountPercentage":10.23,
+"rating":4.43,
+"stock":68,
+"brand":"Microsoft Surface",
+"category":"laptops",
+"thumbnail":"https://i.dummyjson.com/data/products/8/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/8/1.jpg",
+"https://i.dummyjson.com/data/products/8/2.jpg",
+"https://i.dummyjson.com/data/products/8/3.jpg",
+"https://i.dummyjson.com/data/products/8/4.jpg",
+"https://i.dummyjson.com/data/products/8/thumbnail.jpg"]},
+{"id":18,
+"title":"Oil Free Moisturizer 100ml",
+"description":"Dermive Oil Free Moisturizer with SPF 20 is specifically formulated with ceramides, hyaluronic acid & sunscreen.",
+"price":40,
+"discountPercentage":13.1,"rating":4.56,
+"stock":88,
+"brand":"Dermive",
+"category":"skincare",
+"thumbnail":"https://i.dummyjson.com/data/products/18/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/18/1.jpg",
+"https://i.dummyjson.com/data/products/18/2.jpg",
+"https://i.dummyjson.com/data/products/18/3.jpg",
+"https://i.dummyjson.com/data/products/18/4.jpg",
+"https://i.dummyjson.com/data/products/18/thumbnail.jpg"]},
+{"id":19,
+"title":"Skin Beauty Serum.",
+"description":"Product name: rorec collagen hyaluronic acid white face serum riceNet weight: 15 m",
+"price":46,
+"discountPercentage":10.68,
+"rating":4.42,
+"stock":54,
+"brand":"ROREC White Rice",
+"category":"skincare",
+"thumbnail":"https://i.dummyjson.com/data/products/19/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/19/1.jpg",
+"https://i.dummyjson.com/data/products/19/2.jpg",
+"https://i.dummyjson.com/data/products/19/3.png",
+"https://i.dummyjson.com/data/products/19/thumbnail.jpg"]},
+{"id":20,"title":"Freckle Treatment Cream- 15gm",
+"description":"Fair & Clear is Pakistan's only pure Freckle cream which helpsfade Freckles, Darkspots and pigments. Mercury level is 0%, so there are no side effects.",
+"price":70,
+"discountPercentage":16.99,"rating":4.06,
+"stock":140,"brand":"Fair & Clear",
+"category":"skincare",
+"thumbnail":"https://i.dummyjson.com/data/products/20/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/20/1.jpg",
+"https://i.dummyjson.com/data/products/20/2.jpg",
+"https://i.dummyjson.com/data/products/20/3.jpg",
+"https://i.dummyjson.com/data/products/20/4.jpg",
+"https://i.dummyjson.com/data/products/20/thumbnail.jpg"]},
+{"id":21,"title":"- Daal Masoor 500 grams",
+"description":"Fine quality Branded Product Keep in a cool and dry place",
+"price":20,"discountPercentage":4.81,"rating":4.44,"stock":133,
+"brand":"Saaf & Khaas",
+"category":"groceries",
+"thumbnail":"https://i.dummyjson.com/data/products/21/thumbnail.png",
+"images":["https://i.dummyjson.com/data/products/21/1.png",
+"https://i.dummyjson.com/data/products/21/2.jpg",
+"https://i.dummyjson.com/data/products/21/3.jpg"]},
+{"id":28,
+"title":"3D Embellishment Art Lamp","description":"3D led lamp sticker Wall sticker 3d wall art light on/off button  cell operated (included)",
+"price":20,
+"discountPercentage":16.49,"rating":4.82,"stock":54,
+"brand":"LED Lights",
+"category":"home-decoration",
+"thumbnail":"https://i.dummyjson.com/data/products/28/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/28/1.jpg",
+"https://i.dummyjson.com/data/products/28/2.jpg",
+"https://i.dummyjson.com/data/products/28/3.png",
+"https://i.dummyjson.com/data/products/28/4.jpg",
+"https://i.dummyjson.com/data/products/28/thumbnail.jpg"]},
+{"id":30,
+"title":"Key Holder",
+"description":"Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality",
+"price":30,"discountPercentage":2.92,
+"rating":4.92,
+"stock":54,
+"brand":"Golden",
+"category":"home-decoration",
+"thumbnail":"https://i.dummyjson.com/data/products/30/thumbnail.jpg",
+"images":["https://i.dummyjson.com/data/products/30/1.jpg",
+"https://i.dummyjson.com/data/products/30/2.jpg",
+"https://i.dummyjson.com/data/products/30/3.jpg",
+"https://i.dummyjson.com/data/products/30/thumbnail.jpg"]}]
+
+const container=document.querySelector('.container');
+products.map((item)=>{
+
+ container.innerHTML +=`
+ <div class='items'>
+ <h5>${item.category}</h5>
+ <img src="${item.thumbnail}" alt="${item.title}">
+ <h3>${item.title}</h3>
+ <h5>${item.description}</h5>
+ <h6>${item.price}</h6>
+ <h2>${item.rating}</h2>
+ <h3>${item.brand}</h3>
+ <h4>${item.discountPercentage}</h4>
+ 
+ </div>
+ `
+})
+
+
+
+
+const showhere=document.getElementById("showhere");
+const searchbar=document.getElementById("searchbar");
+const Button=document.getElementById("searchbtn");
+let myobj=[]
+Button.addEventListener('click',()=>{
+    let givenvalue=searchbar.value;
+   
+    
+       
+        if(givenvalue!==""){
+           for(const item of products){
+            if(item.category==givenvalue){
+                myobj.push(item);
+            }
+            
+           }
+           showhere.innerHTML="";
+           myobj.map((item)=>{
+           showhere.innerHTML +=` <div class="items">
+          <h5>${item.category}</h5>
+ <img src="${item.thumbnail}" alt="${item.title}">
+ <h3>${item.title}</h3>
+ <h5>${item.description}</h5>
+ <h6>${item.price}</h6>
+ <h2>${item.rating}</h2>
+ <h3>${item.brand}</h3>
+ <h4>${item.discountPercentage}</h4>
+ </div>`
+  })
+     myobj=[];
+        }
+       
+    
+    
+})
+
+
+
+
+
+
